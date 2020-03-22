8c8dc24aae939667baf82a98f6cc991564b0a1ae
Dasha <darya.merkulava@gmail.com>
2020-03-22 18:52:04 +0300

    Any number of brackets

--- src/model/logic/SearchLogic.java
+++ src/model/logic/SearchLogic.java
@@ -3,29 +3,45 @@ package model.logic;
 import java.util.Stack;
 
 public class SearchLogic {
+    final static char[] OPEN_BRACKETS = new char[]{'{', '[', '('};
+    final static char[] CLOSE_BRACKETS = new char[]{'}', ']', ')'};
+
+    private static boolean isInArray(char symbol) {
+
+        for (char openBracket : OPEN_BRACKETS) {
+            if (openBracket == symbol)
+                return true;
+        }
+        return false;
+    }
+
+    private static int indexOf(char symbol) {
+        int index = -1;
+        for (int i = 0; i < CLOSE_BRACKETS.length; i++) {
+            if (CLOSE_BRACKETS[i] == symbol)
+                index = i;
+        }
+        return index;
+    }
 
     public static boolean brackets(String text) {
         if (text == null)
-        throw new IllegalArgumentException();
-
+            throw new IllegalArgumentException();
         Stack<Character> stack = new Stack<>();
-
         for (int i = 0; i < text.length(); i++) {
             char symbol = text.charAt(i);
-            if (symbol == '(' || symbol == '{' || symbol == '[')
+            if (isInArray(symbol))
                 stack.push(symbol);
-            else if (symbol == ']') {
-                if (stack.empty() || stack.pop() != '[')
-                    return false;
-            } else if (symbol == '}') {
-                if (stack.empty() || stack.pop() != '{')
-
-                    return false;
-            } else if (symbol == ')') {
-                if (stack.empty() || stack.pop() != '(')
-                    return false;
+            else {
+                int index = indexOf(symbol);
+                if (index != -1) {
+                    if (stack.empty() || stack.pop() != OPEN_BRACKETS[index])
+                        return false;
+                }
             }
         }
         return stack.empty();
     }
+
+
 }
